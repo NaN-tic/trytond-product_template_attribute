f8d14922664b603628d637d955bfc427f346fd4e
C?dric Krier <ced@b2ck.com>
2018-02-07 10:54:04 +0100

    Activate drone

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
+from . import product
+
 
 def register():
     Pool.register(
-        Template,
+        product.Template,
         module='product_template_attribute', type_='model')
